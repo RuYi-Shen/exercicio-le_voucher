ce5478e46a85fa2e9f9dd837096bf89ea504f5e4
Ru Yi Shen <ruyishen2012@gmail.com>
2022-07-28 18:34:14 -0300

    feat : tests without mock - status ongoing

--- tests/unit/voucherService.test.ts
+++ tests/unit/voucherService.test.ts
@@ -58,3 +58,64 @@ describe("voucherService test suite", () => {
     expect(response).toBeUndefined();
   });
 });
+
+describe("applyVoucher test suite", () => {
+  it("given a valid voucher code should return 200", async () => {
+    const newVoucher = {
+      code: faker.random.alphaNumeric(10),
+      discount: +faker.random.numeric(2),
+    };
+    await voucherService.createVoucher(newVoucher.code, newVoucher.discount);
+    const response = await voucherService.applyVoucher(
+      newVoucher.code,
+      +faker.random.numeric(3)
+    );
+    expect(response.amount).toBeDefined();
+    expect(response.discount).toBeDefined();
+    expect(response.finalAmount).toBeDefined();
+    expect(response.applied).toBeDefined();
+  });
+  it("given a valid voucher code should be used", async () => {
+    const newVoucher = {
+      code: faker.random.alphaNumeric(10),
+      discount: +faker.random.numeric(2),
+    };
+    await voucherService.createVoucher(newVoucher.code, newVoucher.discount);
+    await voucherService.applyVoucher(
+      newVoucher.code,
+      +faker.random.numeric(3)
+    );
+      const response = await prisma.voucher.findUnique({
+        where: { code: newVoucher.code },
+      });
+      expect(response.used).toBe(true);
+  });
+
+  it("given a invalid voucher code should return 400", async () => {
+    const newVoucher = {
+      code: "",
+      discount: +faker.random.numeric(2),
+    };
+    const response = await voucherService.applyVoucher(
+      newVoucher.code,
+      +faker.random.numeric(3)
+    );
+    expect(response).toBe(Error);
+  });
+
+  it("given a low amount should not apply voucher", async () => {
+    const newVoucher = {
+      code: faker.random.alphaNumeric(10),
+      discount: +faker.random.numeric(2),
+    };
+    const response = await voucherService.applyVoucher(
+      newVoucher.code,
+      +faker.random.numeric(2)
+    );
+    expect(response.amount).toBeDefined();
+    expect(response.discount).toBe(0);
+    expect(response.finalAmount).toBe(response.amount);
+    expect(response.applied).toBe(false);
+  });
+
+});
